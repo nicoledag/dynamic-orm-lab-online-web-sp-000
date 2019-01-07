26c3b3251baf583019683f94dd0f1938c16aad17
Nicole D <nnardella@gmail.com>
2019-01-07 15:49:58 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -58,7 +58,7 @@ end
   end
 
   def self.find_by(row)
-    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys[0].to_s} = '#{attribute.values[0].to_s}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{row.keys[0].to_s} = '#{attribute.values[0].to_s}'"
     binding.pry
 
   end
